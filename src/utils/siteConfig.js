77e392576cd70af95bb4d8c4faaf0bc2dce7b123
ceiphr <ari@ceiphr.com>
2020-01-20 13:19:43 -0500

    Updated URL

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://ceiphr.com`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://www.ceiphr.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
