af6d64d90b1be66f7d45b16ec131e000df0ca802
ceiphr <ari@ceiphr.com>
2020-01-20 13:50:01 -0500

    Fixed site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
                 background_color: config.backgroundColor,
                 theme_color: config.themeColor,
                 display: `minimal-ui`,
-                icon: `static/${config.favIcon}`,
+                icon: `static/${config.siteIcon}`,
                 legacy: true,
                 query: `
                 {
